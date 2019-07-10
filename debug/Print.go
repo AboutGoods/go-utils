8e6440175d73f65015ac88cf9aafd1086941d8b7
Henri L <henri.larget@aboutgoods.net>
2019-07-10 14:37:27 +0200

    document of debug.json function

--- debug/Print.go
+++ debug/Print.go
@@ -5,6 +5,8 @@ import (
     "fmt"
 )
 
+//Json print any argument in pretty json indented.
+//Be aware that this will not print the non-exported properties of a structure for example.
 func Json(items ...interface{}){
     j, _:=json.MarshalIndent(items,"","    ")
     fmt.Println(string(j))
