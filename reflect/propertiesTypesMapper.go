b9f7f945c26bfafa371a78ee199fe81f6036dcaa
bduchadeuil <benjamin.duchadeuil@aboutgoods.net>
2019-11-14 09:44:27 +0100

    transform entity to a map of field/values

--- reflect/propertiesTypesMapper.go
+++ reflect/propertiesTypesMapper.go
@@ -20,3 +20,31 @@ func PropertiesTypesMapper(entity interface{}) map[string]string {
 
 	return propTypes
 }
+
+//Returns a map of fields/values from an entity.
+//Nested objects are flatten ( ["var.subVar" => "foo", "var.anotherOne" => "bar"] )
+func GetFlatValuesMap(entity interface{}) map[string]interface{} {
+	value := reflect.ValueOf(entity)
+	valType := value.Type()
+	nbFields := value.NumField()
+
+	valuesMap := make(map[string]interface{})
+
+	for i := 0; i < nbFields; i++ {
+
+		fieldName := valType.Field(i).Name
+
+		if value.Field(i).Kind() != reflect.Struct {
+			valuesMap[fieldName] = value.Field(i)
+		}
+
+		if value.Field(i).Kind() == reflect.Struct {
+			for name, val := range GetFlatValuesMap(value.Field(i).Interface()) {
+				valuesMap[fieldName+"."+name] = val
+			}
+		}
+	}
+
+	return valuesMap
+
+}
