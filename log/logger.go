80c419206be7bf9e8f7a632a605ec5ef1d9ae8c4
Mabboux Jérémy <mabbouxj@gmail.com>
2019-12-16 15:10:00 +0100

    Added mutex to avoid concurrent iteration/writes error on context

--- log/logger.go
+++ log/logger.go
@@ -1,184 +1,256 @@
 // Package log provides global context to logrus logging library
 package log
 
-import "github.com/sirupsen/logrus"
+import (
+	"github.com/sirupsen/logrus"
+	"os"
+	"sync"
+)
 
 var context = logrus.Fields{}
+var mux = sync.Mutex{}
+
+func Init(env string) {
+	logrus.SetOutput(os.Stdout)
+	logrus.SetLevel(logrus.DebugLevel)
+	if env == "prod" {
+		logrus.SetFormatter(&logrus.JSONFormatter{})
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+}
 
 func contextLog() *logrus.Entry {
-
-    return logrus.WithFields(context)
+	return logrus.WithFields(context)
 }
+
 // AddToContext add elements to a global context
 // It will add the key-value pair when calling the other functions available
 func AddToContext(key string, value interface{}) {
-    context[key] = value
+	mux.Lock()
+	defer mux.Unlock()
+	context[key] = value
 }
-// RemoveFromContext remove an element from the global context
-// It will remove on next call from the global context the key passed as argument
-func RemoveFromContext(key string) {
-    delete(context, key)
+
+// RemoveFromContext remove element(s) from the global context
+// It will remove on next call from the global context the key(s) passed as argument
+func RemoveFromContext(keys ...string) {
+	mux.Lock()
+	defer mux.Unlock()
+	for _, k := range keys {
+		delete(context, k)
+	}
 }
 
 // ResetContext will reset the global context of the logger
 func ResetContext() {
-    context = logrus.Fields{}
+	mux.Lock()
+	defer mux.Unlock()
+	context = logrus.Fields{}
 }
 
 // PanicOnError stops the program and logs it if the error passed as the first argument is not nil.
 func PanicOnError(err error, args ...interface{}) {
-    if err != nil {
-        data := append([]interface{}{err}, args...)
-        Fatal(data...)
-    }
+	if err != nil {
+		data := append([]interface{}{err}, args...)
+		Fatal(data...)
+	}
 }
 
 // NoticeOnError if an error occurs, logs it and return true, otherwise false.
-func NoticeOnError(err error, args ...interface{}) bool{
-    if err !=nil{
-        data := append([]interface{}{err}, args...)
-        Info(data...)
-        return true;
-    }
-    return false;
+func NoticeOnError(err error, args ...interface{}) bool {
+	if err != nil {
+		data := append([]interface{}{err}, args...)
+		Info(data...)
+		return true
+	}
+	return false
 }
 
 // Fatal is the global Contextualisation of logrus' eponym function
 func Fatal(args ...interface{}) {
-    contextLog().Fatal(args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Fatal(args...)
 }
 
 // Warning is the global Contextualisation of logrus' eponym function
 func Warning(args ...interface{}) {
-    contextLog().Warning(args)
-
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Warning(args)
 }
 
 // Error is the global Contextualisation of logrus' eponym function
 func Error(args ...interface{}) {
-    contextLog().Error(args...)
-
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Error(args...)
 }
 
 // Panic is the global Contextualisation of logrus' eponym function
 func Panic(args ...interface{}) {
-    contextLog().Panic(args...)
-
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Panic(args...)
 }
 
 // Info is the global Contextualisation of logrus' eponym function
 func Info(args ...interface{}) {
-    contextLog().Info(args...)
-
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Info(args...)
 }
 
 // Debug is the global Contextualisation of logrus' eponym function
 func Debug(args ...interface{}) {
-    contextLog().Debug(args...)
-
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Debug(args...)
 }
 
 // Printf is the global Contextualisation of logrus' eponym function
 func Printf(format string, args ...interface{}) {
-    contextLog().Printf(format, args)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Printf(format, args)
 }
 
 // Trace is the global Contextualisation of logrus' eponym function
 func Trace(args ...interface{}) {
-    contextLog().Trace(args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Trace(args...)
 }
 
 // Print is the global Contextualisation of logrus' eponym function.
 func Print(args ...interface{}) {
-    contextLog().Print(args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Print(args...)
 }
 
 // Warn is the global Contextualisation of logrus' eponym function.
 func Warn(args ...interface{}) {
-    contextLog().Warn(args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Warn(args...)
 }
 
 // Tracef is the global Contextualisation of logrus' eponym function.
 func Tracef(format string, args ...interface{}) {
-    contextLog().Tracef(format, args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Tracef(format, args...)
 }
 
 // Debugf is the global Contextualisation of logrus' eponym function.
 func Debugf(format string, args ...interface{}) {
-    contextLog().Debugf(format, args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Debugf(format, args...)
 }
 
 // Infof is the global Contextualisation of logrus' eponym function.
 func Infof(format string, args ...interface{}) {
-    contextLog().Infof(format, args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Infof(format, args...)
 }
 
 // Warnf is the global Contextualisation of logrus' eponym function.
 func Warnf(format string, args ...interface{}) {
-    contextLog().Warnf(format, args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Warnf(format, args...)
 }
 
 // Warningf is the global Contextualisation of logrus' eponym function.
 func Warningf(format string, args ...interface{}) {
-    contextLog().Warningf(format, args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Warningf(format, args...)
 }
 
 // Errorf is the global Contextualisation of logrus' eponym function.
 func Errorf(format string, args ...interface{}) {
-    contextLog().Errorf(format, args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Errorf(format, args...)
 }
 
 // Panicf is the global Contextualisation of logrus' eponym function.
 func Panicf(format string, args ...interface{}) {
-    contextLog().Panicf(format, args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Panicf(format, args...)
 }
 
 // Fatalf is the global Contextualisation of logrus' eponym function
 func Fatalf(format string, args ...interface{}) {
-    contextLog().Fatalf(format, args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Fatalf(format, args...)
 }
 
 // Traceln is the global Contextualisation of logrus' eponym function.
 func Traceln(args ...interface{}) {
-    contextLog().Traceln(args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Traceln(args...)
 }
 
 // Debugln is the global Contextualisation of logrus' eponym function.
 func Debugln(args ...interface{}) {
-    contextLog().Debugln(args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Debugln(args...)
 }
 
 // Println is the global Contextualisation of logrus' eponym function.
 func Println(args ...interface{}) {
-    contextLog().Println(args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Println(args...)
 }
 
 // Infoln is the global Contextualisation of logrus' eponym function.
 func Infoln(args ...interface{}) {
-    contextLog().Infoln(args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Infoln(args...)
 }
 
 // Warnln is the global Contextualisation of logrus' eponym function.
 func Warnln(args ...interface{}) {
-    contextLog().Warnln(args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Warnln(args...)
 }
 
 // Warningln is the global Contextualisation of logrus' eponym function.
 func Warningln(args ...interface{}) {
-    contextLog().Warningln(args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Warningln(args...)
 }
 
 // Errorln is the global Contextualisation of logrus' eponym function.
 func Errorln(args ...interface{}) {
-    contextLog().Errorln(args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Errorln(args...)
 }
 
 // Panicln is the global Contextualisation of logrus' eponym function.
 func Panicln(args ...interface{}) {
-    contextLog().Panicln(args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Panicln(args...)
 }
 
 // Fatalln is the global Contextualisation of logrus' eponym function
 func Fatalln(args ...interface{}) {
-    contextLog().Fatalln(args...)
+	mux.Lock()
+	defer mux.Unlock()
+	contextLog().Fatalln(args...)
 }
